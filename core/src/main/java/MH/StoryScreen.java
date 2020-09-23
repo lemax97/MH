66567e17b326a8125e099e9263fa2fc9057305da
lemax97 <lemax97@mail.ru>
2020-09-23 21:30:18 +0300

    - tutorial start - added hallway - complete tutorial

--- core/src/main/java/MH/StoryScreen.java
+++ core/src/main/java/MH/StoryScreen.java
@@ -3,8 +3,12 @@ package MH;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputEvent.Type;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class StoryScreen extends BaseScreen {
 
@@ -81,14 +85,158 @@ public class StoryScreen extends BaseScreen {
 
     public void classroom() {
 
+        scene.clearSegments();
+        background.setAnimation( background.classroom);
+        dialogBox.setText("");
+        kelsoe.addAction( SceneActions.moveToOutsideLeft(0) );
+
+        scene.addSegment( new SceneSegment( background, Actions.fadeIn(1) ));
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToScreenCenter(1)));
+        scene.addSegment( new SceneSegment( dialogBox, Actions.show() ));
+
+        addTextSequence( "This is my classroom. My homework isn't here, though." );
+        addTextSequence( "Where should I look for my homework next?" );
+
+        scene.addSegment( new SceneSegment( buttonTable, Actions.show() ));
+
+        // set up options
+        TextButton scienceLabButton = new TextButton("Look in the Science Lab", BaseGame.textButtonStyle);
+        scienceLabButton.addListener(
+                (Event e) -> {
+
+                    if (!(e instanceof InputEvent) ||
+                        !((InputEvent)e).getType().equals(Type.touchDown))
+                        return false;
+                    scene.addSegment( new SceneSegment( buttonTable, Actions.hide() ));
+                    addTextSequence( "That's a great idea. I'll check the science lab." );
+                    scene.addSegment( new SceneSegment( dialogBox, Actions.hide() ));
+                    scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToOutsideLeft(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.fadeOut(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.run(() -> {scienceLab();}) ));
+
+                    return false;
+                }
+        );
+
+        TextButton libraryButton = new TextButton("Look in the Library", BaseGame.textButtonStyle);
+        libraryButton.addListener(
+                (Event e) -> {
+
+                    if ( !(e instanceof InputEvent) ||
+                        !((InputEvent)e).getType().equals(Type.touchDown))
+                        return false;
+                    scene.addSegment( new SceneSegment( buttonTable, Actions.hide() ));
+                    addTextSequence( "That's a great idea. Maybe I left it in the library.");
+                    scene.addSegment( new SceneSegment( dialogBox, Actions.hide() ));
+                    scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToOutsideLeft(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.fadeOut( 1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.run(() -> {library();}) ));
+
+                    return false;
+                }
+        );
+
+        buttonTable.clearChildren();
+        buttonTable.add(scienceLabButton);
+        buttonTable.row();
+        buttonTable.add(libraryButton);
+
+        scene.start();
     }
 
     public void scienceLab() {
 
+        scene.clearSegments();
+
+        background.setAnimation( background.scienceLab);
+        dialogBox.setText("");
+        kelsoe.addAction( SceneActions.moveToOutsideLeft(0));
+
+        scene.addSegment( new SceneSegment( background, Actions.fadeIn(1) ));
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToScreenCenter(1) ));
+        scene.addSegment( new SceneSegment( dialogBox, Actions.show() ));
+
+        addTextSequence( "This is the science lab.");
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.setAnimation( kelsoe.sad )));
+        addTextSequence( "My homework isn't here, though.");
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.setAnimation( kelsoe.normal )));
+        addTextSequence( "Now where should I go?");
+
+        scene.addSegment( new SceneSegment( buttonTable, Actions.show() ));
+
+        // set up options
+        TextButton classroomButton = new TextButton("Return to Classroom", BaseGame.textButtonStyle);
+        classroomButton.addListener(
+                (Event e) -> {
+                    if (!(e instanceof InputEvent) ||
+                    !((InputEvent)e).getType().equals(Type.touchDown))
+                        return false;
+
+                    scene.addSegment( new SceneSegment( buttonTable, Actions.hide() ));
+                    addTextSequence( "Maybe someone found it and put it in the classroom. I'll go check.");
+                    scene.addSegment( new SceneSegment( dialogBox, Actions.hide() ));
+                    scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToOutsideRight(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.fadeOut(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.run(() -> { classroom(); }) ));
+
+                    return false;
+                }
+        );
+
+        TextButton libraryButton = new TextButton("Look in the Library", BaseGame.textButtonStyle);
+        libraryButton.addListener(
+                (Event e) -> {
+
+                    if ( !(e instanceof InputEvent) ||
+                            !((InputEvent)e).getType().equals(Type.touchDown))
+                        return false;
+                    scene.addSegment( new SceneSegment( buttonTable, Actions.hide() ));
+                    addTextSequence( "That's a great idea. Maybe I left it in the library.");
+                    scene.addSegment( new SceneSegment( dialogBox, Actions.hide() ));
+                    scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToOutsideLeft(1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.fadeOut( 1) ));
+                    scene.addSegment( new SceneSegment( background, Actions.run(() -> {library();}) ));
+
+                    return false;
+                }
+        );
+
+        buttonTable.clearChildren();
+        buttonTable.add(classroomButton);
+        buttonTable.row();
+        buttonTable.add(libraryButton);
+
+        scene.start();
     }
 
     public void library() {
+        scene.clearSegments();
 
+        background.setAnimation( background.library);
+        dialogBox.setText("");
+        kelsoe.addAction( SceneActions.moveToOutsideLeft(0));
+
+        scene.addSegment( new SceneSegment( background, Actions.fadeIn(1) ));
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToScreenCenter(1) ));
+        scene.addSegment( new SceneSegment( dialogBox, Actions.show() ));
+
+        addTextSequence( "This is the library.");
+        addTextSequence( "Let me check the table where I was working earlier...");
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.setAnimation( kelsoe.lookRight )));
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToScreenRight(2) ));
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.setAnimation( kelsoe.normal ) ));
+        addTextSequence( "Aha! Here it is!" );
+        scene.addSegment( new SceneSegment( kelsoe, SceneActions.moveToScreenCenter(0.5f) ));
+        addTextSequence( "Thanks for helping me find it!" );
+        scene.addSegment( new SceneSegment( dialogBox, Actions.hide() ));
+
+        scene.addSegment( new SceneSegment( theEnd, Actions.fadeIn(4) ));
+
+        scene.addSegment( new SceneSegment( background, Actions.delay(10) ));
+        scene.addSegment( new SceneSegment( background, Actions.run(
+                () -> { BaseGame.setActiveScreen(new MenuScreen()); }) ));
+
+        scene.start();
     }
 
     @Override
